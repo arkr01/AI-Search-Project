6164a12e9def144deeb63cc92d33231541e635fa
Adrian Rajkamal <88476164+arkr01@users.noreply.github.com>
2022-07-01 15:10:06 +1000

    Update game_env.py

--- game_env.py
+++ game_env.py
@@ -16,9 +16,6 @@ class GameEnv:
 
     The grid is indexed top to bottom, left to right (i.e. the top left corner has coordinates
     (0, 0) and the bottom right corner has coordinates (n_rows-1, n_cols-1)).
-
-    You may use and modify this class however you want. Note that evaluation on GradeScope will
-    use an unmodified GameEnv instance as a simulator.
     """
 
     # input file symbols
